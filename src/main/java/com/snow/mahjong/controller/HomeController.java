422e92f3ccb7a14815241a9290a34f97f6659f7e
“snow624” <snow19980624@gmail.com>
2026-04-29 11:03:30 +0900

    add home controller

---
+++ src/main/java/com/snow/mahjong/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.snow.mahjong.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/ranking";
+    }
+}
